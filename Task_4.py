65cccee93e9955ff25381d37d9477d087de85115
kanan14 <165552783+kanan14@users.noreply.github.com>
2024-04-05 18:01:36 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -1,9 +1,33 @@
 #import libraries here
 
 def main():
-  '''
-  Kodunuzu buraya yazin.
-  '''
+  a=int(input("Enter the year [ex. 2021]: "))
+  if a == 2000:
+    print(a,"is the year of the Dragon")
+  elif a == 2001:
+    print(a,"is the year of the Snake")
+  elif a == 2002:
+    print(a,"is the year of the Horse")
+  elif a == 2003:
+    print(a,"is the year of the Sheep")
+  elif a == 2004:
+    print(a,"is the year of the Monkey")
+  elif a == 2005:
+    print(a,"is the year of the Rooster")
+  elif a == 2006:
+    print(a,"is the year of the Dog")
+  elif a == 2007:
+    print(a,"is the year of the Pig")
+  elif a == 2008:
+    print(a,"is the year of the Rat")
+  elif a == 2009:
+    print(a,"is the year of the Ox")
+  elif a == 2010:
+    print(a,"is the year of the Tiger")
+  elif a == 2011:
+    print(a,"is the year of the Hare")
+  else:
+    print("Invalid year!")
   pass
 
 if __name__ == "__main__":
