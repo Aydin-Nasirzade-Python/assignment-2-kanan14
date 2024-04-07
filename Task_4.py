b20463d3744e319306d394825ac53e17734edfc0
kanan14 <165552783+kanan14@users.noreply.github.com>
2024-04-07 16:59:39 +0400

    Update Task_4.py

--- Task_4.py
+++ Task_4.py
@@ -3,29 +3,29 @@
 def main():
   a=int(input("Enter the year [ex. 2021]: "))
   if a>0:
-    if (a%2000)%12 == 0:
+    if a%12 == 0:
       print(a,"is the year of the Dragon")
-    elif (a%2000)%12==1:
+    elif a%12==1:
       print(a,"is the year of the Snake")
-    elif (a%2000)%12==2:
+    elif a%12==2:
       print(a,"is the year of the Horse")
-    elif (a%2000)%12==3:
+    elif a%12==3:
       print(a,"is the year of the Sheep")
-    elif (a%2000)%12==4:
+    elif a%12==4:
       print(a,"is the year of the Monkey")
-    elif (a%2000)%12==5:
+    elif a%12==5:
       print(a,"is the year of the Rooster")
-    elif (a%2000)%12==6:
+    elif a%12==6:
        print(a,"is the year of the Dog")
-    elif (a%2000)%12==7:
+    elif a%12==7:
       print(a,"is the year of the Pig")
-    elif (a%2000)%12==8:
+    elif a%12==8:
       print(a,"is the year of the Rat")
-    elif (a%2000)%12==9:
+    elif a%12==9:
       print(a,"is the year of the Ox")
-    elif (a%2000)%12==10:
+    elif a%12==10:
       print(a,"is the year of the Tiger")
-    elif (a%2000)%12==11:
+    elif a%12==11:
       print(a,"is the year of the Hare")
     else:
       print("Invalid year!")
